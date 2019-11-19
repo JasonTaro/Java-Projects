071ccee8638c6f0654abae4d39ebd90fdb5eab5a
JasonTaro <42520129+JasonTaro@users.noreply.github.com>
2019-11-18 21:25:05 -0700

    Update Connect4Model.java

--- Connect4/Connect4Model.java
+++ Connect4/Connect4Model.java
@@ -3,7 +3,7 @@ import java.util.Observable;
 /**
  * 
  * @author Jason Fukumoto
- * Description: Model class have the 2d integer array, which represents different color
+ * Model class has 2D integer array, which represents different colors.
  *  0 for white, 1 for yellow, 2 for red
  */
 public class Connect4Model extends Observable{
@@ -13,18 +13,6 @@ public class Connect4Model extends Observable{
 	public Connect4Model() {
 		board = new int [Connect4.ROW][Connect4.COL]; 
 	}
-	 
-	/**
-	 * for board testing, print board state in console
-	 */
-	public void printBoard() {
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 7; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
-		}
-	} 
 	
 	/**
 	 * getter to get color from the board
@@ -35,7 +23,8 @@ public class Connect4Model extends Observable{
 	public int getColor(int row,int col) {
 		return board[row][col];
 	}
-
+	
+	
 	/**
 	 * whenever the view has the user input information, it updates model through 
 	 * controller
