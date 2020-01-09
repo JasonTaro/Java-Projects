702ffcf7f33ed5315846c4181479bf989053e77b
JasonTaro <42520129+JasonTaro@users.noreply.github.com>
2020-01-09 17:26:29 +0900

    Update TowerDefense.java

--- Tower Defense/src/main/TowerDefense.java
+++ Tower Defense/src/main/TowerDefense.java
@@ -2,16 +2,12 @@ package main;
 import MVC.TowerDefenseView;
 import javafx.application.Application;
 /**
- * 
- * @author  YongqiJia & JasonFukumoto & Qingzhou Pan & Guojun Wei
- * CSC 335, Fall 2019
- * Team Project
  * This project is to create a 2-D tower defense game, the player must prevent
  * the enemy from getting to the end with defense towers
- *
  */
 
 public class TowerDefense {	
+	
 	/**
 	 * the main goes directly to the view start method
 	 * @param args
